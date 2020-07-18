4dac007cdf09d509671a9c40621803b679c16ea8
amarsinghen <32318822+amarsinghen@users.noreply.github.com>
2020-07-18 12:48:00 -0700

    Checking in the landmark recognition api

---
+++ app.py
@@ -0,0 +1,53 @@
+'''
+Flask backend for image-to-image search pipeline.
+'''
+
+#Import dependencies
+import os
+import tensorflow as tf
+from inference import simple_inference
+
+#import Flask dependencies
+from flask import Flask, request, render_template, send_from_directory
+
+#Set root dir
+APP_ROOT = os.path.dirname(os.path.abspath(__file__))
+
+#Load model
+trained_model = tf.keras.models.load_model('group5_set224_resnet50_NO_imagenet_weights_07112020.h5')
+
+#Define Flask app
+app = Flask(__name__, static_url_path='/static')
+
+#Define apps home page
+@app.route("/") #www.landmarks_recognition.com/
+def index():
+	return render_template("index.html")
+
+#Define upload function
+@app.route("/upload", methods=["POST"])
+def upload():
+
+	upload_dir = os.path.join(APP_ROOT, "uploads/")
+
+	if not os.path.isdir(upload_dir):
+		os.mkdir(upload_dir)
+
+	for img in request.files.getlist("file"):
+		img_name = img.filename
+		destination = os.path.join(upload_dir, img_name)
+		img.save(destination)
+
+	#inference
+	confidence_score, predicted_class = simple_inference(trained_model, os.path.join(upload_dir, img_name))
+	return render_template("result.html", image_name=img_name, confidence_score=confidence_score, landmark_id_result=predicted_class)
+
+#Define helper function for finding image paths
+@app.route("/upload/<filename>")
+def send_image(filename):
+	return send_from_directory("uploads", filename)
+
+#Start the application
+
+if __name__ == "__main__":
+	app.run(port=5000, debug=True)
---
+++ inference.py
@@ -0,0 +1,32 @@
+import pickle
+import numpy as np
+import pandas as pd
+from PIL import Image, ImageOps
+
+
+def simple_inference(loaded_model,uploaded_image_path):
+
+    '''
+    Doing simple inference for single uploaded image.
+
+    :param loaded_model: CNN model
+    :param uploaded_image_path: string, path to the uploaded image
+    '''
+
+    with open("indices_to_class_labels_dict_group_5.json", "rb") as pickle_file:
+        indices_to_class_labels_dict = pd.read_pickle(pickle_file, compression=None)
+
+    image = resize_image(uploaded_image_path, 224, 224)
+
+    np_image = np.array(image)
+    np_image = np_image/255.0
+    image = np.expand_dims(np_image, axis=0)
+    result_array = loaded_model.predict(image)
+    confidence_score = max(result_array[0]) * 100
+    predicted_class = indices_to_class_labels_dict[np.argmax(result_array)]
+    return confidence_score, predicted_class
+
+def resize_image(image_path, new_width, new_height):
+    image = Image.open(image_path)
+    image = ImageOps.fit(image, (new_width, new_height), Image.ANTIALIAS)
+    return image
