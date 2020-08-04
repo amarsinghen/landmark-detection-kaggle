c3617f6f5b0a14e127347b309cf48663badf63ee
amarsinghen <32318822+amarsinghen@users.noreply.github.com>
2020-08-03 20:46:33 -0700

    updated the support for swagger docs

--- inference.py
+++ inference.py
@@ -5,7 +5,9 @@ import os
 # Since there is no training, we want to run this code only on cpu save gpu resources
 os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
 import tensorflow as tf
+import logging
 
+logging.basicConfig(level=logging.INFO)
 
 def simple_inference(uploaded_image_path):
     '''
@@ -42,9 +44,9 @@ def predict_class_confidence_score(image):
         results_list.append(loaded_models_list[j].predict(image))
         predicted_class_list.append(indices_to_class_labels_dict_list[j][np.argmax(results_list[j])])
         confidence_score_list.append(max(results_list[j][0]))
-        print("Identified landmark for image is : {}".format(predicted_class_list[j]))
-        print("Confidence score : {}".format(confidence_score_list[j]))
-        print("Whole Result array : {}\n".format(sorted(results_list[j][0])[-5:]))
+        logging.info("Identified landmark for image is : {}".format(predicted_class_list[j]))
+        logging.info("Confidence score : {}".format(confidence_score_list[j]))
+        logging.info("Whole Result array : {}\n".format(sorted(results_list[j][0])[-5:]))
         if confidence_score_list[j] > max_confidence_score_tuple[1]:
             max_confidence_score_tuple = (predicted_class_list[j], confidence_score_list[j])
     return max_confidence_score_tuple[0], max_confidence_score_tuple[1]
@@ -57,7 +59,6 @@ def load_model_weights_and_class_labels():
 
     for i in range(num_of_models):
         filepath = "model_weights/group{}_set224_resnet50_weights.hdf5".format(i + 10)
-        print(filepath)
         loaded_models_list.append(tf.keras.models.load_model(filepath))
         with open("model_weights/group{}_indices_to_class_labels_dict.json".format(i + 10), "rb") as pickle_file:
             indices_to_class_labels_dict_list.append(pd.read_pickle(pickle_file))
